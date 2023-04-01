5a0db6288129e51193bc462f20bdbbebffa08a15
Legalize Nuclear Bombs <114302731+legalizenuclearbombs3@users.noreply.github.com>
2023-04-01 11:00:55 -0500

    Delete process-game-recommendations.php

--- process-game-recommendations.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $gameRecommendation = $_POST["game-recommendation"];
-  $file = fopen("game-recommendations.txt", "a");
-  fwrite($file, $gameRecommendation . "\n");
-  fclose($file);
-}
-?>
